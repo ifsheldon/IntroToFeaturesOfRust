0acb30dee428a3a32157bcc176e4aa5061b3d602
maple_workstation <maplevantablack@outlook.com>
2021-04-01 20:57:06 +0300

    added trait example

--- my_fancy_project/src/main.rs
+++ my_fancy_project/src/main.rs
@@ -1,5 +1,44 @@
 use my_fancy_project::module_a::module_a_a;
 
+///
+/// The struct example
+///
+/// Fields are private, so instances cannot be constructed using simple struct syntax
+///
+pub struct People {
+    name: String,
+    age: u8,
+}
+
+pub trait Hello {
+    // immutable function
+    fn get_name(&self) -> String;
+    // mutable function
+    fn change_name(&mut self, new_name: String);
+    // function with default implementation
+    fn say_hello(&self) {
+        println!("Hello!");
+    }
+    // "static" function
+    fn construct_from_name(name: String) -> Self;
+}
+
+impl Hello for People {
+    fn get_name(&self) -> String { self.name.clone() }
+    fn change_name(&mut self, new_name: String) { self.name = new_name; }
+    fn say_hello(&self) { println!("Hi"); }
+    // overriding the default
+    fn construct_from_name(name: String) -> Self {
+        People { name, age: 0 }
+    }
+}
+
+impl<T: Hello> IntroduceSelf for T {
+    fn introduce_self(&self) {
+        println!("Hello, I'm {}", self.get_name());
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     module_a_a::printFromModuleAA();
