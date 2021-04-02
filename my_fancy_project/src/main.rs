f997db71163d590cd63c0b15d41dd7a241058285
ifsheldon <maplevantablack@outlook.com>
2021-04-02 21:17:16 +0300

    added examples
    global var example,
    multi-thread example

--- my_fancy_project/src/main.rs
+++ my_fancy_project/src/main.rs
@@ -1,6 +1,8 @@
 use my_fancy_project::module_a::module_a_a;
 use rayon::prelude::*;
 use std::fmt::Display;
+use std::sync::{Mutex, Arc, MutexGuard};
+use std::thread;
 
 ///
 /// The struct example
@@ -52,12 +54,45 @@ fn print<T: Display>(printable: T) {
     println!("{}", printable);
 }
 
+const CONST_DATA: [u32; 4] = [1, 2, 3, 4];
+const CONST_INT: i32 = 1;
+static CONST_BOOL: bool = false;
+// static IMMUT_STRING : String = String::from("aa");
+static mut MUT_BOOL: bool = true;
+
 fn main() {
     println!("Hello, world!");
     module_a_a::printFromModuleAA();
     let data = vec![1, 2, 3, 4, 5];
     let double: Vec<i32> = data.iter().map(|x| 2 * x).collect();
     let parallel_double: Vec<i32> = data.par_iter().map(|x| 2 * x).collect();
+    println!("{}", CONST_BOOL);
+    let some_bool = unsafe { MUT_BOOL };
+    let numbers = vec![1, 2, 3, 4, 5];
+    for i in 0..numbers.len() {
+        let number_for_sure = unsafe { numbers.get_unchecked(i) };
+        println!("Sure it is {}", number_for_sure);
+    }
+
+    let thread_num = 10;
+    let mut handles = Vec::new();
+    let values_created_in_threads: Vec<i32> = Vec::new();
+    let guarded_values = Mutex::new(values_created_in_threads);
+    let guarded_value_arc = Arc::new(guarded_values);
+    for i in 0..thread_num {
+        let arc_for_one_thread = Arc::clone(&guarded_value_arc);
+        let thread_handle = thread::spawn(move || {
+            let mut vec_guard = arc_for_one_thread.lock().unwrap(); // type is MutexGuard<Vec<i32>>
+            vec_guard.push(i);
+        });
+        handles.push(thread_handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    for i in guarded_value_arc.lock().unwrap().iter() {
+        println!("value : {}", i);
+    }
 }
 
 
@@ -84,19 +119,20 @@ mod tests {
 
     #[test]
     fn closures() {
-        let x = Integer(4);
+        let x = Integer { int: 4 };
         let equal_to_x = |z: Integer| z.int == x.int;
-        let y = Integer(4);
-        let f = &mut y;
+        let y = Integer { int: 4 };
+        ;
+        // let f = &mut y;
         assert!(equal_to_x(y));
         let move_equal_to_x = move |z: Integer| z.int == x.int;
-        let z = Integer(4);
+        let z = Integer { int: 3 };
         assert!(!move_equal_to_x(z));
-        println!("x = {}", x.0); // compile error here "value borrowed here after move"
+        // println!("x = {}", x.int); // compile error here "value borrowed here after move"
     }
 
     fn mut_func1(data: Data) {
-        data.integer.int = 4; // not allowed
+        // data.integer.int = 4; // not allowed
     }
 
     fn mut_func2(mut data: Data) {
