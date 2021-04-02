bb19b27eced10489abedf0008783edc60384eb96
ifsheldon <maplevantablack@outlook.com>
2021-04-02 21:17:30 +0300

    added documentation example

--- my_fancy_project/src/lib.rs
+++ my_fancy_project/src/lib.rs
@@ -1,1 +1,12 @@
 pub mod module_a;
+///
+/// This is a simple add function
+///
+/// # Example
+/// ```
+/// assert_eq!(2, my_fancy_project::add_integers(1, 1));
+/// ```
+///
+pub fn add_integers(x: i32, y: i32) -> i32 {
+    x + y
+}
